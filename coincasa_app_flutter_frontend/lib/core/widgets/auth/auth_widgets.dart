7a30a0c35af0810c6498770fe6a508470a8af76b
Maurocavasinni <cavasinnimauro@gmail.com>
2026-06-18 19:07:03 +0200

    fix: piccola pulizia

--- coincasa_app_flutter_frontend/lib/core/widgets/auth/auth_widgets.dart
+++ coincasa_app_flutter_frontend/lib/core/widgets/auth/auth_widgets.dart
@@ -1,8 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
-import '../../constants/app_sizes.dart';
 import '../../theme/app_theme.dart';
 
 export 'auth_login_logo.dart';
